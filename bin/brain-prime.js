cccbd0bf356169894bbc16c49fe4a557c0cbbc2d
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-02-27 14:12:31 +0300

    step_9

---
+++ bin/brain-prime.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+let correctAnswersCount = 0;
+while (correctAnswersCount < 3) {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
+
+  console.log(`Question: ${randomNumber}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    correctAnswersCount += 1;
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+}
+
+if (correctAnswersCount === 3) {
+  console.log(`Congratulations, ${name}!`);
+}
