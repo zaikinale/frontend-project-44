f7a2809caf81be08c62d33f10b8dbb83635d3565
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-03-04 10:08:24 +0300

    fix_game_progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,3 @@
 // bin/brain-progression.js
-import runGame from '../src/index.js';
-import { description, generateQuestion } from '../src/games/play_progression.js';
-
-runGame(description, generateQuestion);
+import { gameProgression } from '../src/games/play_progression.js'
+gameProgression()
